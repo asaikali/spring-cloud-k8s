486b04bcb762727b6ee8fad2b890d34c6d9d4543
Adib Saikali <asaikali@gmail.com>
2019-11-28 11:55:43 -0500

    Sample works with Spring Cloud K8s

--- message-service/src/main/java/com/example/messageservice/MessageServiceApplication.java
+++ message-service/src/main/java/com/example/messageservice/MessageServiceApplication.java
@@ -3,9 +3,11 @@ package com.example.messageservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableScheduling
 public class MessageServiceApplication {
 
     public static void main(String[] args) {
