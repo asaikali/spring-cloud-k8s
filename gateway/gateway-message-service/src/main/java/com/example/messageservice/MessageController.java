a63de31db4aab18296f51519f87d8bd79364ac7a
dcaron <dcaron@xs4all.nl>
2021-08-26 14:14:01 +0200

    remove unused imports

--- gateway/gateway-message-service/src/main/java/com/example/messageservice/MessageController.java
+++ gateway/gateway-message-service/src/main/java/com/example/messageservice/MessageController.java
@@ -1,7 +1,5 @@
 package com.example.messageservice;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
